399eb0f04184a610d349392ee30d4416c51299a4
shackbei <s.hackbeil@unity-mail.de>
2022-03-03 13:49:03 +0100

    change close postion

--- server/WebServer.cpp
+++ server/WebServer.cpp
@@ -10,7 +10,7 @@
 	
 	SERVER::WebServer::~WebServer()
 	{
-		close(new_socket);
+
 	}
 
 	void SERVER::WebServer::launch()
@@ -30,22 +30,21 @@
 	{
 		sockaddr_in address = get_socket()->get_addr();
 		u_long addrlen = sizeof(address);
-			std::cout << "test" << get_socket()->get_socket_fd() << std::endl;
 		new_socket = accept(get_socket()->get_socket_fd(), (struct sockaddr *)&address, (socklen_t *)&addrlen);
 	}
 
 	void SERVER::WebServer::handler()
 	{
 		t_request request;
-		parse_request(&request, new_socket, buffer);
-		std::string test("HTTP/1.1 200 OK\r\nConnection:	close\r\ncontent-type:	text/html\r\nlast-modified:	Mon, 21 Feb 2022 12:48:28 GMT\r\netag:	\"1a78-62138a1c-215ebfbeb5868481;;;\"\r\naccept-ranges:	bytes\r\ncontent-length:	274\r\ndate:	Wed, 02 Mar 2022 18:14:36 GMT\r\nserver:	LiteSpeed\r\ncontent-security-policy:	upgrade-insecure-requests\r\nalt-svc:	h3=\":443\"; ma=2592000, h3-29=\":443\"; ma=2592000, h3-Q050=\":443\"; ma=2592000, h3-Q046=\":443\"; ma=2592000, h3-Q043=\":443\"; ma=2592000, quic=\":443\"; ma=2592000; v=\"43,46\"\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n    <head>\r\n    <title>Webserv</title>\r\n    </head>\r\n    <body style=\"background-color: black; text-align: center;\">\r\n        <p style=\"padding: 10%; color:aliceblue; font-size: 5rem; \">Hello Webserv</p>\r\n    </body>\r\n</html>");
-    	send(get_new_socket(), (const void *)test.c_str(), test.length(), 0);    
+		parse_request(&request, new_socket, buffer);  
 		std::cout << buffer << std::endl;
 	}
 
 	void SERVER::WebServer::responder()
 	{
-		write(new_socket, "Hello", 5);
+		std::string test("HTTP/1.1 200 OK\r\nConnection:	close\r\ncontent-type:	text/html\r\nlast-modified:	Mon, 21 Feb 2022 12:48:28 GMT\r\netag:	\"1a78-62138a1c-215ebfbeb5868481;;;\"\r\naccept-ranges:	bytes\r\ncontent-length:	274\r\ndate:	Wed, 02 Mar 2022 18:14:36 GMT\r\nserver:	LiteSpeed\r\ncontent-security-policy:	upgrade-insecure-requests\r\nalt-svc:	h3=\":443\"; ma=2592000, h3-29=\":443\"; ma=2592000, h3-Q050=\":443\"; ma=2592000, h3-Q046=\":443\"; ma=2592000, h3-Q043=\":443\"; ma=2592000, quic=\":443\"; ma=2592000; v=\"43,46\"\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n    <head>\r\n    <title>Webserv</title>\r\n    </head>\r\n    <body style=\"background-color: black; text-align: center;\">\r\n        <p style=\"padding: 10%; color:aliceblue; font-size: 5rem; \">Hello Webserv</p>\r\n    </body>\r\n</html>");
+    	send(get_new_socket(), (const void *)test.c_str(), test.length(), 0);
+		close(new_socket);
 	}
 
 	int SERVER::WebServer::get_new_socket()
