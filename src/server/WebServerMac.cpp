8dbc5d7e8024d03eae03511d805ead8a5e305505
shackbei <s.hackbeil@unity-mail.de>
2022-03-09 18:42:04 +0100

    write protected

--- src/server/WebServerMac.cpp
+++ src/server/WebServerMac.cpp
@@ -25,8 +25,7 @@ void SERVER::WebServer::launch()
 	while (42)
 	{
 		tmp_read_sockets = current_sockets;
-		tmp_write_sockets = current_sockets;
-								// tmp copy of fd set, because select function destroys second argument
+		tmp_write_sockets = current_sockets; // tmp copy of fd set, because select function destroys second argument
 		if (select(FD_SETSIZE + 1, &tmp_read_sockets, &tmp_write_sockets, NULL, NULL) < 0) // should a timer be set at 5th argument?
 		{
 			perror("Error");
@@ -40,7 +39,7 @@ void SERVER::WebServer::launch()
 				handler();
 				std::cout << "read" << i << std::endl;
 			}
-			else if(FD_ISSET(i, &tmp_write_sockets)/* && i != listener*/) // fd is ready to be written if true
+			else if(FD_ISSET(i, &tmp_write_sockets)) // fd is ready to be written if true
 			{			
 				tmp_socket_fd = i;
 				std::cout << "weite to " << i << std::endl;
@@ -56,9 +55,7 @@ void SERVER::WebServer::handler()
 	if (tmp_socket_fd == get_socket()->get_socket_fd())
 		handle_new_client();
 	else // data from an existing connection, receive it
-	{
 		handle_known_client();
-	}
 }
 
 void SERVER::WebServer::handle_new_client()
@@ -85,6 +82,8 @@ void SERVER::WebServer::handle_known_client()
 	{
 		std::cout << "Socket closed by client " << tmp_socket_fd << "\n";
 		FD_CLR(tmp_socket_fd, &current_sockets);
+		close(tmp_socket_fd);
+		return;
 	}
 	Parsing info(buffer);
 	std::cout << "insert:" << tmp_socket_fd << std::endl;
@@ -94,7 +93,7 @@ void SERVER::WebServer::handle_known_client()
 void SERVER::WebServer::responder()
 {
 	std::map<int, Parsing>::iterator itr = data.find(tmp_socket_fd);
-	Parsing *info = &itr->second;
+	Parsing *info = &itr->second; // info all header information from client;
 	(void)info;
 	std::string test("HTTP/1.1 200 OK\r\nConnection:	close\r\ncontent-type:	text/html\r\nlast-modified:	Mon, 21 Feb 2022 12:48:28 GMT\r\netag:	\"1a78-62138a1c-215ebfbeb5868481;;;\"\r\naccept-ranges:	bytes\r\ncontent-length:	274\r\ndate:	Wed, 02 Mar 2022 18:14:36 GMT\r\nserver:	LiteSpeed\r\ncontent-security-policy:	upgrade-insecure-requests\r\nalt-svc:	h3=\":443\"; ma=2592000, h3-29=\":443\"; ma=2592000, h3-Q050=\":443\"; ma=2592000, h3-Q046=\":443\"; ma=2592000, h3-Q043=\":443\"; ma=2592000, quic=\":443\"; ma=2592000; v=\"43,46\"\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n    <head>\r\n    <title>Webserv</title>\r\n    </head>\r\n    <body style=\"background-color: black; text-align: center;\">\r\n        <p style=\"padding: 10%; color:aliceblue; font-size: 5rem; \">Hello Webserv</p>\r\n    </body>\r\n</html>");
 	if (send(tmp_socket_fd, (const void *)test.c_str(), test.length(), 0) == -1)
