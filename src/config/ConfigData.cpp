7c06336078a7748244a9da1aa678cf5d6f27784f
FunkyOctopus <>
2022-03-25 10:13:31 +0100

    Default Error Page

--- src/config/ConfigData.cpp
+++ src/config/ConfigData.cpp
@@ -1,7 +1,7 @@
 #include "ConfigData.hpp"
 
-ConfigData::ConfigData(void) : npos(-1), Port(8080),
-							   serverName("UNKNOWN"), errorPage("UNKNOWN"), BodySize(1) {}
+ConfigData::ConfigData(void) : npos(-1), Port(8080), serverName("UNKNOWN"), 
+errorPage("./documents/html_errors./documents/html_errors"), BodySize(1) {}
 
 ConfigData::~ConfigData(void)
 {
