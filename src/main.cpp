1ac052d5ef967572dbea5f36e29911bdef20df7d
vytkuklys <vtts.kuklys@gmail.com>
2022-04-15 02:01:52 +0200

    Added error handling for invalid argv[1]

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,12 @@ void ft_printeror(int a)
 	std::cout << "broken pipe" << std::endl;
 }
 
+bool is_file_exist(const char *fileName)
+{
+    std::ifstream infile(fileName);
+    return infile.good();
+}
+
 int main (int argc, char **argv)
 {
 	if (argc != 2)
@@ -14,9 +20,15 @@ int main (int argc, char **argv)
 		std::cout << "Invalid input" << std::endl;
 		return (-1);
 	}
+	if (!is_file_exist(argv[1]))
+	{
+		std::cout << "Invalid config file" << std::endl;
+		return (-1);
+	}
 	signal(SIGPIPE, ft_printeror);
-	Config *config = new Config(argv[1]);
 
+	Config *config = new Config(argv[1]);
+	// config is deleted in webservermac.cpp
 	SERVER::WebServer webServer(*config);
 	return (0);
 }
