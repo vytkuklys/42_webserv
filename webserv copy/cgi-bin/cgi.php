ebfe378ed43abf222cc97c95b69e23e7d837d269
Samuel Hackbeil <95760252+shackbei@users.noreply.github.com>
2022-04-15 20:39:43 +0200

    Add files via upload

---
+++ webserv copy/cgi-bin/cgi.php
@@ -0,0 +1,65 @@
+#!./php-cgi
+
+<?php
+	// pcntl_signal(SIGINT,SIG_IGN, false);
+	if (isset($_FILES['user_file']) && ($_SERVER['REQUEST_METHOD'] == "POST") && !isset($_ENV['HTTP_COOKIE']))
+		move_uploaded_file($_FILES['user_file']['tmp_name'], '../documents/uploaded/' . $_FILES['user_file']['name']);
+	else if(!isset($_ENV['HTTP_COOKIE']))
+	{
+		$fd = fopen('php://stdin', 'rb');
+		if ($fd === FALSE)
+		{
+			echo "error input\n";
+			phpinfo(INFO_VARIABLES | INFO_ENVIRONMENT);
+			exit("Failed to open stream to URL");
+		}
+		// $file_name = "/goinfre/shackbei/upploads/test";
+		$file_name = $_SERVER['DOCUMENT_ROOT'];
+		// if ($_SERVER['REQUEST_METHOD'] == "POST")
+		// {
+			$i = rand();
+			$file_name .= $i;
+			while(file_exists($file_name))
+			{
+				// fwrite($file_error, $i);
+
+				$file_name = $_SERVER['DOCUMENT_ROOT'] . $i;
+				$i++;
+			}
+		// }
+		// $file_error = fopen("/Users/shackbei/Documents/code/Projects/webserv/documents/uploaded/error", 'w');
+		// fwrite($file_error, $file_name);
+		phpinfo(INFO_VARIABLES | INFO_ENVIRONMENT);
+
+		$out = fopen($file_name, 'w');
+		if ($out === FALSE)
+		{
+			echo "error input\n";
+			phpinfo(INFO_VARIABLES | INFO_ENVIRONMENT);
+			exit("Failed to open out");
+		}
+		$line = '';
+		// while(!feof($fd))
+		while(1)
+		{
+			// fwrite($out, "\n\n===fread========", 16);
+			if(($line = fread($fd, 8192)) === false)
+			{
+				// fwrite($file_error, "\n\n===end========", 16);
+				break;
+			}
+			// fwrite($out, "\n\n===fwrite========", 16);
+			if((fwrite($out, $line, 8192)) === FALSE)
+			{
+				// fwrite($file_error, "\n\n===end========", 16);
+				break;
+			}
+			if(strlen($line) == 0)
+				break;
+		}
+		// fwrite($out, "\n\n===end========", 16);
+		fclose($out);
+		fclose($fd);
+		fclose($file_error);
+	}
+
---
+++ webserv copy/cgi-bin/main.cpp
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	char test[4001];
+	int ret = 1;
+	FILE* tmp;
+	std::string file("/goinfre/pic0.jpg");
+	while ((tmp = fopen(file.c_str(), "wx")) == NULL)
+	{
+		file[30]++;
+	}
+	if(tmp == NULL)
+	{
+		write(STDOUT_FILENO, "STO\n\n", 5);
+		return (1);
+	}
+	int ret_wr;
+	while(ret > 0)
+	{
+		ret = 0;
+		ret_wr = 0;
+		ret = read(STDIN_FILENO, test, 4000);
+		if(ret <= 0)
+			break;
+		ret_wr = fwrite(test , sizeof(char), ret, tmp);
+	}
+	fclose(tmp);
+	return(0);
+}
